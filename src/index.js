8b8d3e848097abecf429beb2e9fa586655474367
Danilov Igor <danilovigor773@gmail.com>
2018-10-11 10:47:56 +0500

    Finished the 22 lesson of js 30. Wrong name of repo. The project about navigation stuff

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 import './index.scss';
 
-console.log("Hello World");
+//console.log("Hello World");
+const triggers = document.querySelectorAll('a');
+
+const highlight = document.createElement('span');
+
+highlight.classList.add('highlight');
+
+document.body.append(highlight);
+
+function highlightLink(){
+  const linkCoords = this.getBoundingClientRect();
+  const deltaX = window.scrollX;
+  const deltaY = window.scrollY;
+  const coords = {
+     left: linkCoords.left + deltaX,
+     top: linkCoords.top + deltaY,
+     width:linkCoords.width,
+     height: linkCoords.height
+  };
+
+  highlight.style.width = `${coords.width}px`;
+  highlight.style.height = `${coords.height}px`;
+  highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+
+}
+
+triggers.forEach(a => a.addEventListener('mouseenter', highlightLink));
